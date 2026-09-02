00763177678f2f5d562958aca4159597800e130b
saharyar khan <saharyarkhan1125@gmail.com>
2026-09-03 00:15:43 +0530

    Simplify index.js, remove static frontend serving

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ connectDB();
 
 const app = express();
 
-// Set CORS for frontend URL / allow single-node deploy
 app.use(cors({
   origin: ['http://localhost:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL],
   credentials: true
