f2792102497fcc58c5c53f6e30c1e64b6ac65c5a
saharyar khan <saharyarkhan1125@gmail.com>
2026-09-03 01:10:41 +0530

    Rename Home page file to avoid case-sensitivity issue

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home';
+import Home from './pages/HomePage';
 import Shop from './pages/Shop';
 import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
@@ -28,7 +28,7 @@ function App() {
       <Navbar />
       <div className="main-content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<home />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/cart" element={<Cart />} />
